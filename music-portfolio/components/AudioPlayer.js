172e3f022458e03e2f24ec18cf7e8d9ef2a70142
David Esposto <desposto@uwo.ca>
2022-02-02 22:43:41 -0500

    Added music player functionality

---
+++ music-portfolio/components/AudioPlayer.js
@@ -0,0 +1,149 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+  FaPlay,
+  FaPause,
+} from "react-icons/fa";
+import { IoShuffleOutline} from "react-icons/io5"
+
+const AudioPlayer = (props) => {
+  console.log(props);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [length, setLength] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const audioPlayer = useRef();
+  const progressBar = useRef();
+  const animationReference = useRef();
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setLength(seconds);
+    progressBar.current.max = seconds;
+    if (isPlaying) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
+  const togglePlayPause = () => {
+    const pastValue = isPlaying;
+    setIsPlaying(!pastValue);
+    if (!pastValue) {
+      audioPlayer.current.play();
+      animationReference.current = requestAnimationFrame(whilePlaying);
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationReference.current);
+    }
+  };
+
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationReference.current = requestAnimationFrame(whilePlaying);
+  };
+
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  };
+
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty(
+      "--seek-before-width",
+      `${(progressBar.current.value / length) * 100}%`
+    );
+    setCurrentTime(progressBar.current.value);
+  };
+
+  const SkipSong = () => {
+    props.setCurrentSongIndex(() => {
+      let temp = props.currentSongIndex;
+      temp++;
+      if (temp > props.songs.length - 1) {
+        temp = 0;
+      }
+      console.log(temp);
+      return temp;
+    });
+  };
+
+  const prevSong = () => {
+    props.setCurrentSongIndex(() => {
+      let temp = props.currentSongIndex;
+      temp--;
+      if (temp < 0) {
+        temp = props.songs.length - 1;
+      }
+      console.log(temp);
+      return temp;
+    });
+  };
+
+  const shuffleSong = () => {
+    let temp = props.currentSongIndex;
+    console.log(temp);
+    let rand = Math.floor(Math.random() * props.songs.length);
+    while (rand == temp) {
+        rand = Math.floor(Math.random() * props.songs.length);
+    }
+      props.setCurrentSongIndex(rand);
+      console.log(rand);
+  };
+
+  return (
+    <div className="flex w-700px">
+      <audio
+        ref={audioPlayer}
+        src={props.songs[props.currentSongIndex].src}
+        preload="metadata"
+      ></audio>
+      <button className="skipButtons" onClick={prevSong}>
+        <FaArrowAltCircleLeft></FaArrowAltCircleLeft>
+      </button>
+      <button
+        onClick={togglePlayPause}
+        className="hover:text-gray-500 flex items-center w-75px h-75px "
+      >
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+      <button className="skipButtons" onClick={SkipSong}>
+        <FaArrowAltCircleRight></FaArrowAltCircleRight>
+      </button>
+      <button className="skipButtons text-2xl" onClick={shuffleSong}>
+        <IoShuffleOutline/>
+      </button>
+      {/*Current Time*/}
+      <div className="font-bold text-base ml-2 mr-2 items-center">
+        {calculateTime(currentTime)}
+      </div>
+      {/*Progress bar*/}
+      <div>
+        <input
+          className="progressBar"
+          type="range"
+          defaultValue="0"
+          ref={progressBar}
+          onChange={changeRange}
+        ></input>
+      </div>
+      {/*Duration*/}
+      <div className="font-bold text-xl ml-2">
+        {length && !isNaN(length) && calculateTime(length)}
+      </div>
+    </div>
+  );
+};
+
+export { AudioPlayer };
--- music-portfolio/pages/index.js
+++ music-portfolio/pages/index.js
@@ -1,8 +1,48 @@
+import React, { useRef, useState, useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { AudioPlayer } from "../components/AudioPlayer";
 
 export default function Home() {
+  const [songs] = useState([
+    {
+      title: "Beach Party",
+      img_src: "",
+      src: "./music/$beach partuy.mp3",
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/$Beauty.mp3",
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/$clk clik cla.mp3",
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/$When we sleep.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +52,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <AudioPlayer
+          currentSongIndex={currentSongIndex}
+          setCurrentSongIndex={setCurrentSongIndex}
+          nextSongIndex={nextSongIndex}
+          songs={songs}
+        ></AudioPlayer>
       </main>
     </div>
   );
