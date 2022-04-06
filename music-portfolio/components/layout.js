f8f69002a6528e59a0a4e61ae2f4a10cdfbc3a7d
David Esposto <desposto@uwo.ca>
2022-04-06 13:22:19 -0400

    added dropdown

--- music-portfolio/components/layout.js
+++ music-portfolio/components/layout.js
@@ -1,10 +1,37 @@
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import React, { useRef, useState, useEffect } from "react";
+import { DropdownMenu } from "../components/DropdownMenu";
 
 export default function Layout({ children }) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  //toggle for Menu
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+  useEffect(() => {
+    const hideDropDownMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    //eventlistener for resize
+    window.addEventListener("resize", hideDropDownMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideDropDownMenu);
+    };
+  });
+
   return (
     <div className="min-h-screen flex flex-col bg-prp items-center">
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <DropdownMenu isOpen={isOpen} toggle={toggle}></DropdownMenu>
       <main className="grow flex items-center">{children}</main>
       <Footer />
     </div>
