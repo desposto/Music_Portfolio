79af11f2ad71032eb38184e7b7dd10afd668bde5
David Esposto <desposto@uwo.ca>
2022-04-06 13:26:04 -0400

    dropdown menu work

--- music-portfolio/components/DropdownMenu.js
+++ music-portfolio/components/DropdownMenu.js
@@ -5,11 +5,11 @@ const DropdownMenu = (props) => {
     <div
       className={
         props.isOpen //if the drop down menu is open desplay if not hide
-          ? "grid grid-rows-3 text-center items-center bg-bk w-full text-wt font-bold font-mono"
+          ? "grid grid-rows-3 text-center items-center bg-bk w-full text-wt font-bold font-mono text-2xl"
           : "hidden"
       }
     >
-      <Link className="p-4" href="/About">
+      <Link className="" href="/About" onClick={props.toggle}>
         About
       </Link>
     </div>
